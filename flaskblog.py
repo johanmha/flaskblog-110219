1b40c9f1640231a53b8b3b6bc307175356d23ff6
Johan Haugland <johan.haugland@evry.com>
2019-02-11 16:20:08 +0100

    Home template variable

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Johan Haugland',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Kari Olsen',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "<h1>About Page </h1>"
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
